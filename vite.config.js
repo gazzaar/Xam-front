88dc6166bd45c4130760e0cbd58477e08694901e
gazzaar <gazzarDev@gmail.com>
2025-02-20 18:52:01 +0200

    use another port

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,9 +9,10 @@ export default defineConfig({
     assetsDir: 'assets',
   },
   server: {
+    port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:5001',
         changeOrigin: true,
         secure: false,
       },
