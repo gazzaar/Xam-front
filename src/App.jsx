c3927a9835a9d79bb55650abd80dc332c9ce07a0
gazzaar <gazzarDev@gmail.com>
2025-02-20 14:14:48 +0200

    Initial app with login and simple landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import { useState } from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Landing from './components/Landing';
+import Login from './components/Login';
 
 function App() {
   return (
-    <>
-      <h1>Hello Graduation project!</h1>
-    </>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/login" element={<Login initialMode="login" />} />
+        <Route path="/signup" element={<Login initialMode="signup" />} />
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </Router>
   );
 }
 
