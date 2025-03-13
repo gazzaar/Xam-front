dfd59984f9a33b0f8b14202d25a07fbbdc3ba9f3
gazzaar <gazzarDev@gmail.com>
2025-03-13 16:39:41 +0200

    Remove every thing and start from scratch

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,5 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Landing from './components/Landing';
-import Login from './components/Login';
-import Register from './components/Register';
-
 function App() {
-  return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Register />} />
-        <Route path="/" element={<Landing />} />
-      </Routes>
-    </Router>
-  );
+  return <h1>Start again</h1>;
 }
 
 export default App;
