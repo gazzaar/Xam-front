bff8590c0e30447517ffe82fcef291fb4e49d915
gazzaar <gazzarDev@gmail.com>
2025-03-20 09:34:12 +0200

    Install and configure tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import tailwindcss from 'tailwindcss';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
