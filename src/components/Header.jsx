20a493f233f5879a97d96d0c249cb554deee6e16
gazzaar <gazzarDev@gmail.com>
2025-03-26 09:21:33 +0200

    add Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import { IoMdCheckboxOutline } from 'react-icons/io';
+const Header = () => {
+  return (
+    <>
+      <header>
+        <div className='p-6 w-3/4 mt-0 mb-0 mr-auto ml-auto  flex items-center justify-between'>
+          <div className='logo flex items-center gap-3'>
+            <h1 className='text-3xl to-emerald-800 font-bold text-slate-800'>
+              Xam
+            </h1>
+            <IoMdCheckboxOutline className='text-2xl text-red-500' />
+          </div>
+          <div className='flex gap-2 items-center '>
+            <button
+              type=''
+              className='text-sm text-white rounded-md border-none outline-solid
+              bg-slate-700 hover:bg-slate-400 transition-all duration-200  border pt-2 pb-2 pr-8 pl-8'
+            >
+              Login
+            </button>
+            <button
+              type=''
+              className='text-sm text-black rounded-md border-solid 
+              outline-solid border-slate-700 hover:bg-slate-700 hover:text-white transition-all duration-200 border pt-2 pb-2 pr-8 pl-8'
+            >
+              Sign up
+            </button>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
