a5eaa0335e2172a98df757ed2cfdeb5d315e724b
gazzaar <gazzarDev@gmail.com>
2025-03-26 10:42:39 +0200

    setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import Header from './components/Header';
 function App() {
-  return <h1>Start again</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/' element={<Header />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
+import { useNavigate } from 'react-router-dom';
 import { IoMdCheckboxOutline } from 'react-icons/io';
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <>
       <header>
@@ -12,6 +14,7 @@ const Header = () => {
           </div>
           <div className='flex gap-2 items-center '>
             <button
+              onClick={() => navigate('/login')}
               type=''
               className='text-sm text-white rounded-md border-none outline-solid
               bg-slate-700 hover:bg-slate-400 transition-all duration-200  border pt-2 pb-2 pr-8 pl-8'
@@ -19,6 +22,7 @@ const Header = () => {
               Login
             </button>
             <button
+              onClick={() => navigate('/signup')}
               type=''
               className='text-sm text-black rounded-md border-solid 
               outline-solid border-slate-700 hover:bg-slate-700 hover:text-white transition-all duration-200 border pt-2 pb-2 pr-8 pl-8'
