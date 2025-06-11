3ca11e68b337a2b9f2d770c7e4f87a5795336c30
gazzaar <gazzarDev@gmail.com>
2025-06-11 13:27:03 +0300

    change numbers

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -444,7 +444,7 @@ export default function LandingPage() {
                       d='M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z'
                     />
                   </svg>
-                  Up to 50 Exams
+                  Up to 20 Exams
                 </li>
               </ul>
               <Link
@@ -519,7 +519,7 @@ export default function LandingPage() {
                       d='M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z'
                     />
                   </svg>
-                  Up to 300 Exams
+                  Up to 450 Exams
                 </li>
               </ul>
               <Link
