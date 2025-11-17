0d2241326886d0f5b3a65b7e504c7cb2d80fdf1e
gazzaar <gazzarDev@gmail.com>
2025-11-17 17:16:34 +0200

    add dotenv

--- src/components/instructor/ExamPreview.jsx
+++ src/components/instructor/ExamPreview.jsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
 import { useNavigate, useParams } from "react-router-dom";
+import dotenv from "dotenv";
+dotenv.config();
 
 const ExamPreview = () => {
   const { id } = useParams();
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
+import dotenv from "dotenv";
+dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
